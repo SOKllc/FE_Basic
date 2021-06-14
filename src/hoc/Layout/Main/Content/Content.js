ee3b49fff28eb404bd131bb31b1c38b0e860076a
Salman <salmankunbargi@gmail.com>
2021-06-14 21:10:20 +0300

    Adding Layout

---
+++ src/hoc/Layout/Main/Content/Content.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classes from "./Content.module.css";
+
+const content = (props) => {
+  return <div className={classes.Content}>Content</div>;
+};
+
+export default content;
---
+++ src/hoc/Layout/Main/Main.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Content from "./Content/Content";
+
+const main = (props) => {
+  return <Content />;
+};
+
+export default main;
