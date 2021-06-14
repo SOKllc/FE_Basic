82e8770df000c51a8c42bb4ad72652e2fdd7abd4
SOK <salmankunbargi@gmail.com>
2021-06-15 01:14:57 +0300

    Adding Footer

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -4,12 +4,14 @@ import classes from "./Layout.module.css";
 
 import Header from "./Header/Header";
 import Main from "./Main/Main";
+import Footer from "./Footer/Footer";
 class Layout extends Component {
   render() {
     return (
       <div className={classes.Layout}>
         <Header />
         <Main />
+        <Footer />
       </div>
     );
   }
