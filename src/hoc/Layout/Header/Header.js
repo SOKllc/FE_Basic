9886c3568efd44086c1ee97de8821f5fe3af5859
SOK <salmankunbargi@gmail.com>
2021-06-15 01:08:47 +0300

    Adding Header

---
+++ src/hoc/Layout/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classes from "./Header.module.css";
+
+const header = (props) => {
+  return <div className={classes.Header}>Header</div>;
+};
+
+export default header;
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 
 import classes from "./Layout.module.css";
 
+import Header from "./Header/Header";
 import Main from "./Main/Main";
 class Layout extends Component {
   render() {
     return (
       <div className={classes.Layout}>
+        <Header />
         <Main />
       </div>
     );
